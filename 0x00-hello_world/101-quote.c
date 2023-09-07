05f82d7c94565c832a92a6845c59db4d11c01c4b
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-08 02:54:29 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,10 +12,10 @@ int main(void)
 ssize_t bytes_written = write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 if (bytes_written == -1)
-    {
+{
 perror("write");
 return (EXIT_FAILURE);
-    }
+}
 
 return (1);
 }
