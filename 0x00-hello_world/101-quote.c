93e9f8025465534b65606d83cd2e583714c0fdd9
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-08 02:52:35 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-	    ssize_t bytes_written = write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    ssize_t bytes_written = write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	        if (bytes_written == -1)
-			    {
-				            perror("write");
-					            return (EXIT_FAILURE);
-						        }
+    if (bytes_written == -1)
+    {
+        perror("write");
+        return (EXIT_FAILURE);
+    }
 
-		    return (1);
+    return (1);
 }
-
