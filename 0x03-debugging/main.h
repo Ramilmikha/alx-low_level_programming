49964bfe2989c85aacdb4313ba8d3d9806cc2efc
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-15 00:38:30 +0300

    Update main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+// Function prototype for the positive_or_negative() function
+void positive_or_negative(int i);
+
+// Function prototype for the largest_number() function
+int largest_number(int a, int b, int c);
+
+// Function prototype for the convert_day() function
+int convert_day(int month, int day);
+
+// Function prototype for the print_remaining_days() function
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
