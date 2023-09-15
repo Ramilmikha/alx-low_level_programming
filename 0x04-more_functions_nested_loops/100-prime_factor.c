9ac8f031f6371c13fc8ab65c7bb16bc6aeb5b96d
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-15 16:54:55 +0300

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - finds and prints the largest prime factor of 612852475143
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+long number = 612852475143;
+long largest_prime = -1;
+int i;
+while (number % 2 == 0)
+{
+largest_prime = 2;
+number /= 2;
+}
+for (i = 3; i <= sqrt(number); i += 2)
+{
+while (number % i == 0)
+{
+largest_prime = i;
+number /= i;
+}
+}
+if (number > 2)
+largest_prime = number;
+printf("%ld\n", largest_prime);
+return (0);
+}
