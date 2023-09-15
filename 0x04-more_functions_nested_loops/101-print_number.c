87f80737202b2d7a65676238b2390a6133e9e75c
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-15 16:58:09 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,12 +6,28 @@
  */
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-if (n / 10 != 0)
-print_number(n / 10);
-_putchar((n % 10) + '0');
+    int divisor = 1;
+    int is_negative = 0;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        is_negative = 1;
+    }
+
+    while (n / divisor > 9 || n / divisor < -9)
+        divisor *= 10;
+
+    while (divisor != 0)
+    {
+        int digit = n / divisor;
+
+        if (is_negative)
+            _putchar((digit * -1) + '0');
+        else
+            _putchar(digit + '0');
+
+        n %= divisor;
+        divisor /= 10;
+    }
 }
