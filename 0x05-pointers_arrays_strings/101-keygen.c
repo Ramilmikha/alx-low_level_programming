19396ee69977486989a4899ae6b9391d446b98d0
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-19 02:49:31 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,28 +1,20 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #define PASSWORD_LENGTH 12
 int main(void)
 {
-int i;  
+int i;
 char password[PASSWORD_LENGTH + 1]; /* +1 for the null terminator */
 const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 const int charset_length = sizeof(charset) - 1;
 srand(time(NULL));
-for ( i = 0; i < PASSWORD_LENGTH; i++)
+for (i = 0; i < PASSWORD_LENGTH; i++)
 {
 int random_index = rand() % charset_length;
 password[i] = charset[random_index];
 }
 password[PASSWORD_LENGTH] = '\0';
-if (strcmp(password, "Tada! Congrats") == 0)
-{
-printf("Tada! Congrats\n");
-}
-else
-{
-printf("Wrong password\n");
-}
+printf("%s\n", password);
 return (0);
 }
