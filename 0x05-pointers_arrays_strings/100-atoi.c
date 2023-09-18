15d9f1f6d278ba78b1abe61cf6776ff95a2ef26c
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-19 02:20:30 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,13 +3,15 @@
  * _atoi - Converts a string to an integer.
  * @s: The string to be converted.
  *
- * Return: The integer value of the string.
+ * Return: The integer value of the string, or 0 if no valid integer is found.
  */
 int _atoi(char *s)
 {
 int sign = 1;
 int result = 0;
 int i = 0;
+if (s == NULL)
+return 0;
 if (s[0] == '-')
 {
 sign = -1;
@@ -19,6 +21,13 @@ for (; s[i] != '\0'; i++)
 {
 if (s[i] >= '0' && s[i] <= '9')
 {
+if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+{
+if (sign == 1)
+return INT_MAX;
+else
+return INT_MIN;
+}
 result = result * 10 + (s[i] - '0');
 }
 else
