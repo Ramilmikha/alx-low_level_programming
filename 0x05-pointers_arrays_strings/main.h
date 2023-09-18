c9e347caae84b9bda9479e6e7fedd023c9fdc256
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-19 01:09:29 +0300

    Create main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Exercise 0: reset_to_98 */
+void reset_to_98(int *n);
+
+/* Exercise 1: swap_int */
+void swap_int(int *a, int *b);
+
+/* Exercise 2: _strlen */
+int _strlen(char *s);
+
+/* Exercise 3: _puts */
+void _puts(char *str);
+
+/* Exercise 4: print_rev */
+void print_rev(char *s);
+
+/* Exercise 5: rev_string */
+void rev_string(char *s);
+
+/* Exercise 6: puts2 */
+void puts2(char *str);
+
+/* Exercise 7: puts_half */
+void puts_half(char *str);
+
+/* Exercise 8: print_array */
+void print_array(int *a, int n);
+
+/* Exercise 9: _strcpy */
+char *_strcpy(char *dest, char *src);
+
+#endif /* MAIN_H */
