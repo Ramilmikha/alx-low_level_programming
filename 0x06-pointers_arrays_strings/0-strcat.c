240f15e918a4f029127141851e4c314ba6342f91
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-22 01:33:09 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,25 +7,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int dest_len = 0;
-    int i = 0;
-
-    // Calculate the length of the destination string
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
-
-    // Append characters from src to dest
-    while (src[i] != '\0')
-    {
-        dest[dest_len] = src[i];
-        dest_len++;
-        i++;
-    }
-
-    // Add null terminator to the end of the concatenated string
-    dest[dest_len] = '\0';
-
-    return dest;
+int dest_len = 0;
+int i = 0;
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
+while (src[i] != '\0')
+{
+dest[dest_len] = src[i];
+dest_len++;
+i++;
+}
+dest[dest_len] = '\0';
+return dest;
 }
