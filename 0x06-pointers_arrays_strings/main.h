5e449963b84ee210b80a229032bc4e08be22ddab
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-22 01:47:30 +0300

    Create main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Task 0 */
+char *_strcat(char *dest, char *src);
+
+/* Task 1 */
+char *_strncat(char *dest, char *src, int n);
+
+/* Task 2 */
+char *_strncpy(char *dest, char *src, int n);
+
+/* Task 3 */
+int _strcmp(char *s1, char *s2);
+
+/* Task 4 */
+void reverse_array(int *a, int n);
+
+/* Task 5 */
+char *string_toupper(char *str);
+
+/* Task 6 */
+char *cap_string(char *str);
+
+/* Task 7 */
+char *leet(char *str);
+
+#endif /* MAIN_H */
