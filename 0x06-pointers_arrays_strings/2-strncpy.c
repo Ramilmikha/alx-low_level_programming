a99bec25e2afbbf94daad2cd1f91d20e6740a52d
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-22 01:40:12 +0300

    Update 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+/**
+* _strncpy - Copies a string, up to 'n' bytes.
+* @dest: The destination string.
+* @src: The source string.
+* @n: The maximum number of bytes to copy from src.
+*
+* Return: A pointer to the resulting string `dest`.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return dest;
+}
