0552c88bddc5000ecf9840627c0f049d4bb421c7
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-22 01:36:44 +0300

    Update 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+/**
+* _strncat - Concatenates two strings, using at most 'n' bytes from src.
+* @dest: The destination string.
+* @src: The source string.
+* @n: The maximum number of bytes from src to concatenate.
+* Return: A pointer to the resulting string `dest`.
+*/
+char *_strncat(char *dest, char *src, int n)
+{int dest_len = 0;
+int i = 0;
+while (dest[dest_len] != '\0')
+{dest_len++;}
+while (src[i] != '\0' && i < n)
+{dest[dest_len] = src[i];
+dest_len++;
+i++;}
+dest[dest_len] = '\0';
+return dest;}
