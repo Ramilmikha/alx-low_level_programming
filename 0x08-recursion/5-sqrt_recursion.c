a21a7140a8200cd5727615d57dca00f114e7931e
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-29 04:52:17 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,9 @@
 #include "main.h"
 /**
-* _sqrt_recursion - Returns the natural square root of a number, recursively.
-* @n: The number to find the square root of.
-* Return: The square root of n, or -1 if it doesn't have a natural square root.
-*/
-int find_sqrt(n, guess + 1)
-{
-if (n < 0)
-return -1;
-if (n == 0 || n == 1)
-return n;
-return find_sqrt(n, 1);
-}
-/**
-* find_sqrt - Helper function to find the square root recursively.
+* find_sqrt - Recursive helper function to find the square root.
 * @n: The number to find the square root of.
 * @guess: The current guess for the square root.
-* Return: The square root of n, or -1 if it doesn't have a natural square root.
+* Return: The square root of n, or -1 if n does not have a natural square root.
 */
 int find_sqrt(int n, int guess)
 {
@@ -25,4 +12,15 @@ return guess;
 if (guess * guess > n)
 return -1;
 return find_sqrt(n, guess + 1);
+}
+/**
+* _sqrt_recursion - Returns the natural square root of a number.
+* @n: The number to find the square root of.
+* Return: The square root of n, or -1 if n does not have a natural square root.
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return -1;
+return find_sqrt(n, 1);
 }
