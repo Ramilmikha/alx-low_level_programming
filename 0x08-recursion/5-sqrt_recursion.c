7411c71b8234ccb824394b6f5333693e8077b68d
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-29 05:33:06 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,10 +8,10 @@
 int find_sqrt(int n, int guess)
 {
 if (guess * guess == n)
-return guess;
+return (guess);
 if (guess * guess > n)
-return -1;
-return find_sqrt(n, guess + 1);
+return (-1);
+return (find_sqrt(n, guess + 1));
 }
 /**
 * _sqrt_recursion - Returns the natural square root of a number.
@@ -21,6 +21,6 @@ return find_sqrt(n, guess + 1);
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-return -1;
-return find_sqrt(n, 1);
+return (-1);
+return (find_sqrt(n, 1));
 }
