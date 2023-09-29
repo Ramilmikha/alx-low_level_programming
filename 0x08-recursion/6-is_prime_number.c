67823d61c851c8e7e473816ba64d8c8b507a72d8
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-29 05:01:19 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,11 +6,11 @@
 */
 int is_prime_number(int n)
 {
-if (n <= 1) // Non-prime cases
+if (n <= 1)
 return 0;
-if (n <= 3) // Prime cases: 2 and 3
+if (n <= 3)
 return 1;
-return check_prime(n, 2); // Start checking divisors from 2
+return check_prime(n, 2);
 }
 /**
 * check_prime - Helper function to check if a number is prime recursively.
@@ -20,9 +20,9 @@ return check_prime(n, 2); // Start checking divisors from 2
 */
 int check_prime(int n, int divisor)
 {
-if (divisor * divisor > n) // Checked all possible divisors
+if (divisor * divisor > n)
 return 1;
-if (n % divisor == 0) // Found a divisor, not prime
+if (n % divisor == 0)
 return 0;
-return check_prime(n, divisor + 1); // Recursively check the next divisor
+return check_prime(n, divisor + 1);
 }
