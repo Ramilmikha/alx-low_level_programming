afa253d17b7a6c76d4617092a3ff85f79cd1089c
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-09-29 05:14:07 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,7 @@ int is_palindrome_helper(char *s, int start, int end);
 int is_palindrome(char *s)
 {
 int len = strlen(s);
-return is_palindrome_helper(s, 0, len - 1);
+return (is_palindrome_helper(s, 0, len - 1));
 }
 /**
 * is_palindrome_helper - Recursive helper function to check if a string is a palindrome.
@@ -21,8 +21,8 @@ return is_palindrome_helper(s, 0, len - 1);
 int is_palindrome_helper(char *s, int start, int end)
 {
 if (start >= end)
-return 1;
+return (1);
 if (s[start] != s[end])
-return 0;
-return is_palindrome_helper(s, start + 1, end - 1);
+return (0);
+return (is_palindrome_helper(s, start + 1, end - 1));
 }
