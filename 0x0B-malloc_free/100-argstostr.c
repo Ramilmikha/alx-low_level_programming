de93983b36a1937f9dd052515de2a903975f7ce0
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-04 00:18:18 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,46 +1,46 @@
 #include <stdlib.h>
-
+/**
+* argstostr - Concatenates an array of strings into a single string.
+* @ac: The number of strings in the array.
+* @av: An array of strings to be concatenated.
+* Return: A pointer to a newly allocated string containing the concatenated
+*strings, separated by newline characters ('\n'). Returns NULL if
+*ac is 0, av is NULL, or if memory allocation fails.
+*/
 char *argstostr(int ac, char **av)
 {
-    
-    
-    int i, j;
-    char *concatenated;
-    int pos = 0;
-    int total_length = 0;
-
-    if (ac == 0 || av == NULL)
-        return (NULL);
-
-    for (i = 0; i < ac; i++)
-    {
-        j = 0;
-        while (av[i][j] != '\0')
-        {
-            total_length++;
-            j++;
-        }
-        total_length++;
-    }
-
-    concatenated = (char *)malloc((total_length + 1) * sizeof(char));
-    if (concatenated == NULL)
-        return (NULL);
-
-    pos = 0;
-    for (i = 0; i < ac; i++)
-    {
-        j = 0;
-        while (av[i][j] != '\0')
-        {
-            concatenated[pos] = av[i][j];
-            pos++;
-            j++;
-        }
-        concatenated[pos] = '\n';
-        pos++;
-    }
-
-    concatenated[pos] = '\0';
-    return (concatenated);
+int i, j;
+char *concatenated;
+int pos = 0;
+int total_length = 0;
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+j = 0;
+while (av[i][j] != '\0')
+{
+total_length++;
+j++;
+}
+total_length++;
+}
+concatenated = (char *)malloc((total_length + 1) * sizeof(char));
+if (concatenated == NULL)
+return (NULL);
+pos = 0;
+for (i = 0; i < ac; i++)
+{
+j = 0;
+while (av[i][j] != '\0')
+{
+concatenated[pos] = av[i][j];
+pos++;
+j++;
+}
+concatenated[pos] = '\n';
+pos++;
+}
+concatenated[pos] = '\0';
+return (concatenated);
 }
