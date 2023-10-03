ab16150b00e4e75650848bf7fdf13b83ac495c74
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-03 20:15:22 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,21 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>  // Include the string.h library for strlen
+/**
+* str_concat - Concatenates two strings.
+* @s1: The first string.
+* @s2: The second string.
+* Return: If memory allocation fails or if either s1 or s2 is NULL, returns NULL.
+* Otherwise, returns a pointer to the concatenated string.
+*/
 char *str_concat(char *s1, char *s2)
 {
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-int len1 = _strlen(s1);
-int len2 = _strlen(s2);
+int len1 = strlen(s1);  // Use strlen from string.h
+int len2 = strlen(s2);  // Use strlen from string.h
 int total_len = len1 + len2;
 char *concatenated = (char *)malloc((total_len + 1) * sizeof(char));
 if (concatenated == NULL)
 return (NULL);
-for (int i = 0; i < len1; i++)
+int i;
+for (i = 0; i < len1; i++)
 concatenated[i] = s1[i];
-for (int i = 0; i < len2; i++)
-concatenated[len1 + i] = s2[i];
+for (int j = 0; j < len2; j++)  // Use a different variable for the second loop
+concatenated[len1 + j] = s2[j];
 concatenated[total_len] = '\0';
 return (concatenated);
 }
