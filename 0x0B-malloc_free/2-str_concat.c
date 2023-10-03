ca129af581b5c372c42223181560f2b0aec1c2ea
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-03 22:24:22 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,18 +14,17 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-int len1 = strlen(s1);
-int len2 = strlen(s2);
-int total_len = len1 + len2;
+int len1, len2, total_len, i;
+len1 = strlen(s1);
+len2 = strlen(s2);
+total_len = len1 + len2;
 char *concatenated = (char *)malloc((total_len + 1) * sizeof(char));
-int i;
 if (concatenated == NULL)
 return (NULL);
 for (i = 0; i < len1; i++)
 concatenated[i] = s1[i];
-int j;
-for (j = 0; j < len2; j++)
-concatenated[len1 + j] = s2[j];
+for (int j = 0; j < len2; j++)
+concatenated[i + j] = s2[j];
 concatenated[total_len] = '\0';
 return (concatenated);
 }
