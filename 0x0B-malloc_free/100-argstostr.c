4e815579256f27e73ece2cfae5a889611aedf9bb
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-04 00:12:06 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,7 +18,7 @@ char *argstostr(int ac, char **av)
             total_length++;
             j++;
         }
-        total_length++;  // Add 1 for the newline character
+        total_length++;
     }
 
     concatenated = (char *)malloc((total_length + 1) * sizeof(char));
@@ -35,10 +35,10 @@ char *argstostr(int ac, char **av)
             pos++;
             j++;
         }
-        concatenated[pos] = '\n';  // Add a newline character between arguments
+        concatenated[pos] = '\n';
         pos++;
     }
 
-    concatenated[pos] = '\0';  // Null-terminate the concatenated string
+    concatenated[pos] = '\0';
     return (concatenated);
 }
