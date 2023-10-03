002fbd34a83180e02a951460d89764ddaf29cc18
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-03 23:54:18 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,9 +4,10 @@ char *argstostr(int ac, char **av)
 if (ac == 0 || av == NULL)
 return (NULL);
 int total_length = 0;
-for (int i = 0; i < ac; i++)
+int i, j;
+for (i = 0; i < ac; i++)
 {
-int j = 0;
+j = 0;
 while (av[i][j] != '\0')
 {
 total_length++;
@@ -18,9 +19,9 @@ char *concatenated = (char *)malloc((total_length + 1) * sizeof(char));
 if (concatenated == NULL)
 return (NULL);
 int pos = 0;
-for (int i = 0; i < ac; i++)
+for (i = 0; i < ac; i++)
 {
-int j = 0;
+j = 0;
 while (av[i][j] != '\0')
 {
 concatenated[pos] = av[i][j];
@@ -28,7 +29,6 @@ pos++;
 j++;
 }
 concatenated[pos] = '\n';
-pos++;
 }
 concatenated[pos] = '\0';
 return (concatenated);
