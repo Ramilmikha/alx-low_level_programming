028ec37b78987641730a7f19847f5aba2e325267
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-04 00:03:36 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,36 +1,43 @@
 #include <stdlib.h>
+
 char *argstostr(int ac, char **av)
 {
-int total_length, i, j,  pos;
-char *concatenated;
-if (ac == 0 || av == NULL)
-return (NULL);
-total_length = 0;
-for (i = 0; i < ac; i++)
-{
-j = 0;
-while (av[i][j] != '\0')
-{
-total_length++;
-j++;
-}
-total_length++;
-}
-*concatenated = (char *)malloc((total_length + 1) * sizeof(char));
-if (concatenated == NULL)
-return (NULL);
-pos = 0;
-for (i = 0; i < ac; i++)
-{
-j = 0;
-while (av[i][j] != '\0')
-{
-concatenated[pos] = av[i][j];
-pos++;
-j++;
-}
-concatenated[pos] = '\n';
-}
-concatenated[pos] = '\0';
-return (concatenated);
+    if (ac == 0 || av == NULL)
+        return (NULL);
+
+    int total_length = 0;
+    int i, j;
+
+    for (i = 0; i < ac; i++)
+    {
+        j = 0;
+        while (av[i][j] != '\0')
+        {
+            total_length++;
+            j++;
+        }
+        total_length++;  // Add 1 for the newline character
+    }
+
+    char *concatenated;
+    concatenated = (char *)malloc((total_length + 1) * sizeof(char));
+    if (concatenated == NULL)
+        return (NULL);
+
+    int pos = 0;
+    for (i = 0; i < ac; i++)
+    {
+        j = 0;
+        while (av[i][j] != '\0')
+        {
+            concatenated[pos] = av[i][j];
+            pos++;
+            j++;
+        }
+        concatenated[pos] = '\n';  // Add a newline character between arguments
+        pos++;
+    }
+
+    concatenated[pos] = '\0';  // Null-terminate the concatenated string
+    return (concatenated);
 }
