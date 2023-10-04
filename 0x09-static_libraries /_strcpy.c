ba1cd52b7f583490d551846d1775aed6e3c4252e
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-05 02:11:45 +0300

    Create _strcpy.c

---
+++ 0x09-static_libraries /_strcpy.c
@@ -0,0 +1,14 @@
+/* _strcpy.c */
+
+char *_strcpy(char *dest, char *src)
+{
+    char *temp = dest;
+    while (*src)
+    {
+        *temp = *src;
+        temp++;
+        src++;
+    }
+    *temp = '\0';
+    return dest;
+}
