9a0be01a8c36687f0394827ff7d29414804f6260
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-05 05:50:06 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,50 +1,45 @@
 #include "main.h"
-
 /**
- * is_digit - Checks if a string is a positive number
- * @str: The string to check
- * Return: 1 if it's a positive number, 0 otherwise
- */
+* is_digit - Checks if a string is a positive number
+* @str: The string to check
+* Return: 1 if it's a positive number, 0 otherwise
+*/
 int is_digit(char *str)
 {
-    if (!str || *str == '\0')
-        return (0);
-
-    while (*str)
-    {
-        if (!isdigit(*str))
-            return (0);
-        str++;
-    }
-
-    return (1);
+if (!str || *str == '\0')
+return (0);
+while (*str)
+{
+if (!isdigit(*str))
+return (0);
+str++;
 }
-
+return (1);
+}
 /**
- * main - Adds positive numbers
- * @argc: The number of arguments
- * @argv: An array of arguments
- * Return: 0 on success, 1 on error
- */
+* main - Adds positive numbers
+* @argc: The number of arguments
+* @argv: An array of arguments
+* Return: 0 on success, 1 on error
+*/
 int main(int argc, char *argv[])
 {
-    int sum = 0;
-    int i;
-
-    for (i = 1; i < argc; i++)
-    {
-        if (is_digit(argv[i]))
-        {
-            int num = atoi(argv[i]);
-            sum += num;
-        }
-        else
-        {
-            printf("Error\n");
-            return (1);
-        }
-    }
-
-    printf("%d\n", sum);
-    return (0);
+int num, i, sum;
+sum = 0;
+int i;
+for (i = 1; i < argc; i++)
+{
+if (is_digit(argv[i]))
+{
+num = atoi(argv[i]);
+sum += num;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", sum);
+return (0);
 }
