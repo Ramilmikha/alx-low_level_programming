89daf253fd25d3a99e2579638648748c656b716a
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-05 05:31:28 +0300

    Update main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int _putchar(char c);
+int is_digit(char *str);
+int get_sum_of_args(int argc, char *argv[]);
+
+#endif /* MAIN_H */
