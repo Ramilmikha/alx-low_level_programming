489f70fb3caf9e993e6588971e7387826c1e6999
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-07 02:58:20 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,46 +1,21 @@
 #include <stdlib.h>
-#include <stdio.h>
-
+#include "main.h"
 /**
- * _calloc - Allocates memory for an array using malloc.
- * @nmemb: Number of elements in the array.
- * @size: Size of each element.
- * Return: A pointer to the allocated memory.
- */
+* _calloc - Allocates memory for an array using malloc.
+* @nmemb: Number of elements in the array.
+* @size: Size of each element.
+* Return: A pointer to the allocated memory.
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
-    unsigned int i;
-
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-
-    ptr = malloc(nmemb * size);
-
-    if (ptr == NULL)
-        return (NULL);
-
-    for (i = 0; i < nmemb * size; i++)
-        *((char *)ptr + i) = 0;
-
-    return (ptr);
-}
-
-int main(void)
-{
-    char *a;
-
-    a = _calloc(98, sizeof(char));
-    if (a == NULL)
-        return (1);
-
-    a[95] = 'H';
-    a[96] = 'o';
-    a[97] = 'l';
-    a[98] = '\0';
-
-    printf("%s\n", a);
-    free(a);
-
-    return (0);
+void *ptr;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+return (NULL);
+for (i = 0; i < nmemb * size; i++)
+*((char *)ptr + i) = 0;
+return (ptr);
 }
