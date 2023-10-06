a492f4452caf89cba29075cac1785877ab49e029
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-07 02:46:49 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,11 +1,5 @@
-/*
-* File: 100-realloc.c
-* Author: Peter Njoroge
-*/
-
 #include "main.h"
 #include <stdlib.h>
-
 /**
 *  _realloc - creates an array of integers.
 *  @ptr: ptr arg
@@ -13,32 +7,30 @@
 *  @new_size: int arg
 *  Return: Nothing
 */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p;
-	unsigned int i, n = new_size;
-	char *oldp = ptr;
-
-	if (ptr == NULL)
-	{
-		p = malloc(new_size);
-		return (p);
-	}
-	else if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	else if (new_size == old_size)
-		return (ptr);
-	p = malloc(new_size);
-	if (p == NULL)
-		return (NULL);
-	if (new_size > old_size)
-		n = old_size;
-	for (i = 0; i < n; i++)
-		p[i] = oldp[i];
-	free(ptr);
-	return (p);
+char *p;
+unsigned int i, n = new_size;
+char *oldp = ptr;
+if (ptr == NULL)
+{
+p = malloc(new_size);
+return (p);
+}
+else if (new_size == 0)
+{
+free(ptr);
+return (NULL);
+}
+else if (new_size == old_size)
+return (ptr);
+p = malloc(new_size);
+if (p == NULL)
+return (NULL);
+if (new_size > old_size)
+n = old_size;
+for (i = 0; i < n; i++)
+p[i] = oldp[i];
+free(ptr);
+return (p);
 }
