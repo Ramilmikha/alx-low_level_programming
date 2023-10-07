4192080199926f62c9e5abe9e471812c2781fc45
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-07 03:07:53 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,3 +14,21 @@ str++;
 }
 return (1);
 }
+/**
+* main - Multiplies two positive numbers.
+* @argc: The number of command-line arguments.
+* @argv: An array of command-line argument strings.
+* Return: 0 on success, 98 on error.
+*/
+int main(int argc, char *argv[])
+{
+if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+{
+printf("Error\n");
+return (98);
+}
+unsigned long num1 = strtoul(argv[1], NULL, 10);
+unsigned long num2 = strtoul(argv[2], NULL, 10);
+printf("%lu\n", num1 * num2);
+return (0);
+}
