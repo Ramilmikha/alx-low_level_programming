7b4549859d7fab270cffc87d1f19c6b23c90b7c4
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-07 03:01:05 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,45 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-
 /**
- * array_range - Creates an array of integers.
- * @min: Minimum value.
- * @max: Maximum value.
- * Return: A pointer to the newly created array.
- */
+* array_range - Creates an array of integers.
+* @min: Minimum value.
+* @max: Maximum value.
+* Return: A pointer to the newly created array.
+*/
 int *array_range(int min, int max)
 {
-    int *arr;
-    int size, i;
-
-    if (min > max)
-        return (NULL);
-
-    size = max - min + 1;
-    arr = malloc(size * sizeof(int));
-
-    if (arr == NULL)
-        return (NULL);
-
-    for (i = 0; i < size; i++)
-        arr[i] = min++;
-
-    return (arr);
-}
-
-int main(void)
-{
-    int *a;
-    int i;
-
-    a = array_range(0, 10);
-    if (a == NULL)
-        return (1);
-
-    for (i = 0; i <= 10; i++)
-        printf("%d\n", a[i]);
-
-    free(a);
-
-    return (0);
+int *arr;
+int size, i;
+if (min > max)
+return (NULL);
+size = max - min + 1;
+arr = malloc(size * sizeof(int));
+if (arr == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+arr[i] = min++;
+return (arr);
 }
