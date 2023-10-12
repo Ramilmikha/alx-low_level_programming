ae686d2d1c9b8dfc5a655f1ea25178bda82e2ad7
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-12 04:56:18 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,15 +1,12 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
+#include <stddef.h>
 /**
 * struct op - Struct op
 * @op: The operator
 * @f: The function associated
 */
-typedef struct op
-{
-char *op;
-int (*f)(int a, int b);
-} op_t;
-int (*get_op_func(char *s))(int, int);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /* FUNCTION_POINTERS_H */
-unsigned char *main_opcodes(int num_bytes);
