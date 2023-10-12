010975c0959e57aeed2f4c55c872558b2d7a4802
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-12 04:58:56 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,36 +14,8 @@ if (array != NULL && cmp != NULL && size > 0)
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]))
-return i;
+return (i);
 }
 }
-return -1;
-}
-/**
-* is_98 - Checks if an integer is equal to 98
-* @elem: The integer to check
-*
-* Return: 1 if true, 0 if false
-*/
-int is_98(int elem)
-{
-return (elem == 98);
-}
-/**
-* is_strictly_positive - Checks if an integer is greater than 0
-* @elem: The integer to check
-* Return: 1 if true, 0 if false
-*/
-int is_strictly_positive(int elem)
-{
-return (elem > 0);
-}
-/**
-* abs_is_98 - Checks if the absolute value of an integer is 98
-* @elem: The integer to check
-* Return: 1 if true, 0 if false
-*/
-int abs_is_98(int elem)
-{
-return (elem == 98 || elem == -98);
+return (-1);
 }
