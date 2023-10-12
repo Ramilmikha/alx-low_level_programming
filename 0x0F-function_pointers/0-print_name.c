beb7a0649506da5cc9a6b571b30d8cfaf8a30c4d
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-12 04:51:57 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,37 +5,8 @@
 * @name: The name to print
 * @f: A pointer to a function that takes a char pointer and returns void
 */
-zoid print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-(name != NULL && f != NULL)
+if (name != NULL && f != NULL)
 f(name);
-}
-/**
-* print_name_as_is - Prints a name as is
-* @name: The name of the person
-*/
-void print_name_as_is(char *name)
-{
-printf("Hello, my name is %s\n", name);
-}
-/**
-* print_name_uppercase - Prints a name in uppercase
-* @name: The name of the person
-*/
-void print_name_uppercase(char *name)
-{
-unsigned int i = 0;
-printf("Hello, my uppercase name is ");
-while (name[i])
-{
-if (name[i] >= 'a' && name[i] <= 'z')
-{
-  putchar(name[i] + 'A' - 'a');
-}
-else
-{
- putchar(name[i]);
- }
-i++;
-}
 }
